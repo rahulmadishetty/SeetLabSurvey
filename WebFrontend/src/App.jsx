99588c0ae49a4576ada0699309eedc5ecc182d50
rahulmadishetty <madir01@pfw.edu>
2024-04-18 00:25:08 -0400

    Minor change make the code run

--- WebFrontend/src/App.jsx
+++ WebFrontend/src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignInForm from './components/SignInForm';
 import SignUpForm from './components/SignUpForm';
 import Dashboard from './Dashboard';
-import TakeSurvey from './user/TakeSurvey';
-import ReportPage from './user/ReportPage';
 import './styles.css'; // Ensure this is the correct path to your styles
 
 function App() {
@@ -17,8 +15,6 @@ function App() {
           <Route path="/" element={<SignInForm />} />
           <Route path="/signup" element={<SignUpForm />} />
           <Route path="/dashboard" element={<Dashboard role={userRole} />} />
-          <Route path="/take-survey/:surveyId" element={<TakeSurvey />} />
-          <Route path="/reports/:surveyId" element={<ReportPage />} />
         </Routes>
       </div>
     </Router>
